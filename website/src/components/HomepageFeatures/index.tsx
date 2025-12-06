c0502dd181d04e423b2dbf23d6ad288a140c1fac
suhail <mygamingchanal786@gmail.com>
2025-12-07 04:08:19 +0500

    new commnet

--- website/src/components/HomepageFeatures/index.tsx
+++ website/src/components/HomepageFeatures/index.tsx
@@ -11,13 +11,12 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: " Physical AI & Humanoid Robotics",
-    Svg: require("img/download.png").default,
+    title: "Easy to Use",
+    Svg: require("@site/static/img/undraw_docusaurus_mountain.svg").default,
     description: (
       <>
-        This course provides a comprehensive introduction to Physical AI and
-        Humanoid Robotics, covering essential concepts, tools, and techniques to
-        build intelligent robotic systems that interact with the physical world.
+        Docusaurus was designed from the ground up to be easily installed and
+        used to get your website up and running quickly.
       </>
     ),
   },
@@ -28,16 +27,6 @@ const FeatureList: FeatureItem[] = [
       <>
         Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
         ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: "Powered by React",
-    Svg: require("@site/static/img/undraw_docusaurus_react.svg").default,
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
       </>
     ),
   },
